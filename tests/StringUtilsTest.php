e7acf2252ea08a295224179e6ac5e2668e8c06e2
xelvhk <khlex93@gmail.com>
2024-07-02 13:41:12 +0300

    first commit

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require("src/StringUtils.php");
+
+if (StringUtils\capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (StringUtils\capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
